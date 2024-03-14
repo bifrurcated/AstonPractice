0b0bbd8d0c2688cd9f0a481d78a8873fe3d6127e
bifurcated <litvinenko-maks@inbox.ru>
2024-03-14 21:44:38 +0300

    Add example LazyInitializationException and Fix it

--- src/test/java/com/github/bufrurcated/astonpractice/service/EmployeeServiceTest.java
+++ src/test/java/com/github/bufrurcated/astonpractice/service/EmployeeServiceTest.java
@@ -1,11 +1,14 @@
 package com.github.bufrurcated.astonpractice.service;
 
 import com.github.bufrurcated.astonpractice.dao.EmployeeDAO;
+import com.github.bufrurcated.astonpractice.dao.EmployeeWithDepartmentsDAO;
 import com.github.bufrurcated.astonpractice.db.ConfigurationDB;
 import com.github.bufrurcated.astonpractice.entity.Department;
 import com.github.bufrurcated.astonpractice.entity.Employee;
 import com.github.bufrurcated.astonpractice.entity.PhoneNumber;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.LazyInitializationException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +16,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+@Slf4j
 public class EmployeeServiceTest {
     private EmployeeService employeeService;
+    private EmployeeService employeeWithDepartmentsService;
     private ConfigurationDB configuration;
 
     @SneakyThrows
@@ -23,6 +28,8 @@ public class EmployeeServiceTest {
         configuration = new ConfigurationDB();
         var dao = new EmployeeDAO(configuration.getSessionFactory());
         employeeService = new EmployeeService(dao);
+
+        employeeWithDepartmentsService = new EmployeeService(new EmployeeWithDepartmentsDAO(configuration.getSessionFactory()));
     }
 
     @AfterEach
@@ -53,5 +60,44 @@ public class EmployeeServiceTest {
                 List.of(phoneNumber),
                 List.of(new Department(1L, "Programmer")));
         Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testLazyInitializationException() throws Exception {
+        var employee = new Employee();
+        employee.setFirstName("Nick");
+        employee.setLastName("Malkovich");
+        employee.setAge(21);
+        var phoneNumber = new PhoneNumber();
+        phoneNumber.setPhoneNumber("89005551123");
+        phoneNumber.setEmployee(employee);
+        var department = new Department();
+        department.setName("Programmer");
+        employee.setDepartments(List.of(department));
+        employeeService.add(employee);
+
+        var result = employeeService.getById(1L);
+        Assertions.assertThrows(LazyInitializationException.class, () -> {
+            result.getDepartments().getFirst();
+        });
+    }
+
+    @Test
+    public void testFixLazyInitializationException() throws Exception {
+        var employee = new Employee();
+        employee.setFirstName("Nick");
+        employee.setLastName("Malkovich");
+        employee.setAge(21);
+        var phoneNumber = new PhoneNumber();
+        phoneNumber.setPhoneNumber("89005551123");
+        phoneNumber.setEmployee(employee);
+        var department = new Department();
+        department.setName("Programmer");
+        employee.setDepartments(List.of(department));
+        employeeWithDepartmentsService.add(employee);
+
+        var emp = employeeWithDepartmentsService.getById(1L);
+        var departmentName = emp.getDepartments().getFirst().getName();
+        Assertions.assertEquals("Programmer", departmentName);
     }
 }
